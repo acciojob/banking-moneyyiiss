e79fde03d5028a43788ec44ed4ea2759b67877bc
Manish Mani Dinkar <moneyyiiss@gmail.com>
2023-07-27 09:59:03 +0530

    my answer

--- src/main/java/com/driver/BankAccount.java
+++ src/main/java/com/driver/BankAccount.java
@@ -3,29 +3,52 @@ package com.driver;
 public class BankAccount {
 
     private String name;
-    private double balance;
+    protected double balance;
     private double minBalance;
 
     public BankAccount(String name, double balance, double minBalance) {
-
+        this.name = name;
+        this.balance = balance;
+        this.minBalance = minBalance;
+    }
+    public BankAccount(String name, double balance){
+        this.name = name;
+        this.balance = balance;
     }
 
     public String generateAccountNumber(int digits, int sum) throws Exception{
         //Each digit of an account number can lie between 0 and 9 (both inclusive)
         //Generate account number having given number of 'digits' such that the sum of digits is equal to 'sum'
         //If it is not possible, throw "Account Number can not be generated" exception
+        int sum1 = 0;
+        int dummy = digits;
+        while(dummy != 0){
+            int f = dummy % 10;
+            dummy /= 10;
+            sum1+=dummy;
 
-        return null;
+        }
+        if(sum1 != sum){
+            throw new Exception("Account Number can not be generated");
+        }
+        return "Your account number is generated";
     }
 
     public void deposit(double amount) {
         //add amount to balance
+        this.balance +=amount;
+        System.out.println(amount + "has been deposited in your account");
 
     }
 
     public void withdraw(double amount) throws Exception {
         // Remember to throw "Insufficient Balance" exception, if the remaining amount would be less than minimum balance
-
+        if(balance-amount < minBalance){
+            throw new Exception("Insufficient Balance");
+        }else{
+            this.balance-=amount;
+            System.out.println(amount + "has been withdrawn. Remaining Balance is " + this.balance);
+        }
     }
 
 }
--- src/main/java/com/driver/CurrentAccount.java
+++ src/main/java/com/driver/CurrentAccount.java
@@ -1,19 +1,80 @@
 package com.driver;
 
+import java.util.Arrays;
+import java.util.HashMap;
+
 public class CurrentAccount extends BankAccount{
     String tradeLicenseId; //consists of Uppercase English characters only
 
     public CurrentAccount(String name, double balance, String tradeLicenseId) throws Exception {
+        super(name, balance);
+        this.tradeLicenseId = tradeLicenseId;
+        if(balance < 5000) throw new Exception("Insufficient Balance");
         // minimum balance is 5000 by default. If balance is less than 5000, throw "Insufficient Balance" exception
 
     }
 
     public void validateLicenseId() throws Exception {
+
         // A trade license Id is said to be valid if no two consecutive characters are same
         // If the license Id is valid, do nothing
         // If the characters of the license Id can be rearranged to create any valid license Id
         // If it is not possible, throw "Valid License can not be generated" Exception
+        char[] chars = tradeLicenseId.toCharArray();
+        if(!isValid(tradeLicenseId)){
+            // if false, then this is not valid
+            throw new Exception("Valid License can not be generated");
+        }else{
+            // if not valid then try to valid.
+            String validId = generateValidLicenseId(tradeLicenseId);
+        }
 
+    }
+    public static String generateValidLicenseId(String licenseId) throws Exception {
+        char[] chars = licenseId.toCharArray();
+        Arrays.sort(chars);
+
+        String sortedLicenseId = new String(chars);
+
+
+        int n = sortedLicenseId.length();
+        StringBuilder validLicenseId = new StringBuilder();
+        int left = 0;
+        int right = (n % 2 == 0) ? n / 2 : n / 2 + 1;
+// ABBCCDD
+        // ACBDBD
+        while (right < n) {
+            validLicenseId.append(sortedLicenseId.charAt(left));
+            validLicenseId.append(sortedLicenseId.charAt(right));
+            left++;
+            right++;
+        }
+
+        if (n % 2 != 0) {
+            validLicenseId.append(sortedLicenseId.charAt(left));
+        }
+
+
+
+        return validLicenseId.toString();
+    }
+    public boolean isValid(String str){
+        int count = 0;
+        HashMap<Character, Integer> hm = new HashMap<>();
+        int i = 0;
+        int len = str.length();
+        while(i < len){
+            char ch = str.charAt(i);
+            hm.put(ch, hm.getOrDefault(ch,0)+1);
+            if(hm.size() != 0 && hm.get(ch) > count){
+                count = hm.get(ch);
+            }
+            i++;
+        }
+        if(count >= len/2){
+            return false;
+        }
+        return true;
     }
 
 }

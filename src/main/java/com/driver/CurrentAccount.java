2bbcf6e7102abf02c8415cac8da8137eafcc5241
Manish Mani Dinkar <moneyyiiss@gmail.com>
2023-08-02 02:09:18 +0530

    updated six time

--- src/main/java/com/driver/CurrentAccount.java
+++ src/main/java/com/driver/CurrentAccount.java
@@ -15,7 +15,7 @@ public class CurrentAccount extends BankAccount{
     }
 
     public CurrentAccount(String name, double balance, String tradeLicenseId) throws Exception {
-        super(name, balance);
+        super(name, balance, 5000);
         this.tradeLicenseId = tradeLicenseId;
         if(balance < 5000) throw new Exception("Insufficient Balance");
         // minimum balance is 5000 by default. If balance is less than 5000, throw "Insufficient Balance" exception
@@ -28,61 +28,80 @@ public class CurrentAccount extends BankAccount{
         // If the license Id is valid, do nothing
         // If the characters of the license Id can be rearranged to create any valid license Id
         // If it is not possible, throw "Valid License can not be generated" Exception
-        char[] chars = tradeLicenseId.toCharArray();
-        if(!isValid(tradeLicenseId)){
-            // if false, then this is not valid
-            throw new Exception("Valid License can not be generated");
-        }else{
-            // if not valid then try to valid.
-            String validId = generateValidLicenseId(tradeLicenseId);
-        }
-
-    }
-    public static String generateValidLicenseId(String licenseId) throws Exception {
-        char[] chars = licenseId.toCharArray();
-        Arrays.sort(chars);
-
-        String sortedLicenseId = new String(chars);
-
-
-        int n = sortedLicenseId.length();
-        StringBuilder validLicenseId = new StringBuilder();
-        int left = 0;
-        int right = (n % 2 == 0) ? n / 2 : n / 2 + 1;
-// ABBCCDD
-        // ACBDBD
-        while (right < n) {
-            validLicenseId.append(sortedLicenseId.charAt(left));
-            validLicenseId.append(sortedLicenseId.charAt(right));
-            left++;
-            right++;
-        }
-
-        if (n % 2 != 0) {
-            validLicenseId.append(sortedLicenseId.charAt(left));
-        }
-
-
-
-        return validLicenseId.toString();
-    }
-    public boolean isValid(String str){
-        int count = 0;
-        HashMap<Character, Integer> hm = new HashMap<>();
-        int i = 0;
-        int len = str.length();
-        while(i < len){
-            char ch = str.charAt(i);
-            hm.put(ch, hm.getOrDefault(ch,0)+1);
-            if(hm.size() != 0 && hm.get(ch) > count){
-                count = hm.get(ch);
+        if(!isLicenseNumberValid(tradeLicenseId)){
+            String rearrangeId = rearrangeString(tradeLicenseId);
+            if(rearrangeId == ""){
+                throw new Exception("Valid License can not be generated");
+            }else{
+                this.tradeLicenseId = rearrangeId;
             }
-            i++;
         }
-        if(count >= len/2){
-            return false;
+
+    }
+    public char getMaxCountChar(int[] count){
+        int max = 0;
+        char ch = 0;
+        for(int i = 0; i<26; i++){
+            if(count[i] > max){
+                max = count[i];
+                ch = (char)((int)'A' + i);
+            }
+        }
+        return ch;
+    }
+
+    public String rearrangeString(String s){
+        int N = s.length();
+
+        int[] count = new int[26];
+        for(int i=0; i<26; i++){
+            count[i] = 0;
+        }
+
+        for(char ch : s.toCharArray()){
+            count[(int)ch - (int)'A']++;
+        }
+
+        char ch_max = getMaxCountChar(count);
+        int maxCount = count[(int)ch_max - (int)'A'];
+
+        if(maxCount > (N + 1)/2)
+            return "";
+
+        String res = "";
+        for(int i=0; i<N; i++){
+            res += ' ';
+        }
+
+        int ind = 0;
+        while(maxCount > 0){
+            res = res.substring(0, ind) + ch_max +
+                    res.substring(ind + 1);
+            ind = ind + 2;
+            maxCount--;
+        }
+        count[(int)ch_max - (int)'A'] = 0;
+        for(int i=0; i<26; i++){
+            while(count[i] > 0){
+                ind = (ind >= N) ? 1 : ind;
+                res = res.substring(0, ind) +
+                        (char)((int)'A' + i)
+                        + res.substring(ind + 1);
+                ind += 2;
+                count[i]--;
+            }
+        }
+        return res;
+    }
+    public boolean isLicenseNumberValid(String licenseId){
+        for(int i=0; i<licenseId.length() - 1; i++){
+            if(licenseId.charAt(i) == licenseId.charAt(i+1)){
+                return false;
+            }
         }
         return true;
     }
+
+
 
 }
